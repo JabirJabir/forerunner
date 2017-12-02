ad01854e1324db56e76f33c356d3b2e7dde9bf50
JabirJabir <jabir95tsai@gmail.com>
2017-12-02 17:58:16 +0800

    1202

--- forerunner/js/main.js
+++ forerunner/js/main.js
@@ -3,18 +3,20 @@ var db = fdb.db("school");
 
 $(document).ready(function() {
 	studentCollection.load(dataLoad);
-	$("#table-tb").on("click",".col",colClick);
+	$("#table-tb").on("click",".dataID",colIDClick);
+	$("#table-tb").on("click",".btn-danger",btnDeleteClick);
+	// $("#table-tb").on("click","dataID",colIDClick);
 })
 
 var studentCollection = db.collection("students");
-var newStudent = {
-    name: "Koding",
-    age: 18
-};
-studentCollection.insert(newStudent);
+// var newStudent = {
+    // name: "Koding",
+    // age: 18
+// };
+// studentCollection.insert(newStudent);
 console.log(studentCollection.load());
 function dataLoad() {
-	console.log("data loaded")
+	// console.log("data loaded")
 	console.log(studentCollection.find());
 	// cb();
 	updateTable(studentCollection.find());
@@ -25,8 +27,8 @@ function cb() {
 	// update(studentCollection.find());
 }
 function dataSave() {
-	console.log("data saved");
-	studentCollection.save();
+	// console.log("data saved");
+	updateTable(studentCollection.find());
 }
 
 function createData() {
@@ -50,28 +52,39 @@ function updateTable(datas) {
 			"<td>" + (i+1) + "</td>"+
 			"<td class = 'dataID'>" + datas[i]._id + "</td>"+
 			"<td>" + datas[i].name + "</td>"+
+			"<td><button class = 'btn btn-warning'>修改</button>" + "	" +
+			"<button class = 'btn btn-danger'>刪除</button></td>" +
 			"</tr>"
 		);
 	}
 }
 
-function colClick() {
-	$("#modal-body").find("p").remove();
-	console.log("colClick");
-	var ID = $(this).find(".dataID").text();
+function colIDClick() {
+	$("#modal-body").find("p").remove(); 
+	console.log("colIDClick");
+	var ID = $(this).text();
 	var query = {
 	    _id: ID
 	};
 	var studentData = studentCollection.find(query);
 	$("#modal-body").append(
-		"<p>ID" + studentData[0]._id + "</p>" +
-		"<p>Name" + studentData[0].name + "</p>" +
-		"<p>Age" + studentData[0].age + "</p>"
+		"<p>ID : " + studentData[0]._id + "</p>" +
+		"<p>Name : " + studentData[0].name + "</p>" +
+		"<p>Age : " + studentData[0].age + "</p>"
 		);
 	$("#modal").modal("show");
 }
 
-
+function btnDeleteClick(){
+	if (confirm("確定要刪除嗎?") == false) {
+		return;
+	}
+	var di = $(this).closest("tr").find(".dataID").text();
+	studentCollection.remove({
+	    _id: di
+	});
+	studentCollection.save(dataSave);
+}
 
 
 
